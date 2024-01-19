583644622df9469c29827f2b00d4c9509da99215
Farah065 <farahahmad065@gmail.com>
2024-01-19 21:06:47 +0200

    Added background, avatar image, and intro text

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Avatar from './Images/Avatar.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello :3</h1>
+    <div className='App'>
+      <div className='avatarBorder'>
+        <img src={Avatar} height={350} className='avatar'/>
+      </div>
+      <div className='introText'>
+        <h2>Hey! I'm</h2>
+        <h1>Farah</h1>
+      </div>
     </div>
   );
 }
