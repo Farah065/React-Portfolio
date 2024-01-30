a82c3959f2528a13c7e67478e8c58f9751e41c96
Farah065 <farahahmad065@gmail.com>
2024-01-30 19:23:44 +0200

    finished projects section

--- portfolio/src/Components/Card.js
+++ portfolio/src/Components/Card.js
@@ -3,11 +3,12 @@ function Card(props) {
         <div className='card-container'>
             <div className={"card-outline " + props.col}>
                 <div className='card'>
-                    <div className='image'>
-
+                    <div>
+                        <img src={props.img} className='card-img'/>
                     </div>
                     <div className='card-text'>
-                        
+                        <h1>{props.title}</h1>
+                        <p>{props.desc}</p>
                     </div>
                 </div>
             </div>
--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -1,4 +1,8 @@
 import Card from './Card';
+import calcImg from './Images/Calc.png';
+import lemonImg from './Images/Lemon.png';
+import oathboundImg from './Images/Oathbound.png';
+import tlouImg from './Images/TLOU.png';
 
 const oathbound = ["Oathbound", "Game created using the Unity game engine in 2 days for the Global Game Jam (2023), along with 8 other mentors in the university’s game development club."];
 const calc = ["React Calc", "Functional calculator created using React JS for the Meta front-end developer professional certificate on Coursera."];
@@ -10,12 +14,12 @@ function Projects() {
         <div id='projects'>
             <div className='card-group'>
                 <div>
-                    <Card col='col-1' />
-                    <Card col='col-1' />
+                    <Card col='col-1' title={oathbound[0]} desc={oathbound[1]} img={oathboundImg}/>
+                    <Card col='col-1' title={tlou[0]} desc={tlou[1]} img={tlouImg}/>
                 </div>
                 <div>
-                    <Card col='col-2' />
-                    <Card col='col-2' />
+                    <Card col='col-2' title={calc[0]} desc={calc[1]} img={calcImg}/>
+                    <Card col='col-2' title={lemon[0]} desc={lemon[1]} img={lemonImg}/>
                 </div>
             </div>
         </div>
