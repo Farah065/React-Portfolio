d9381807e841be54f504947cc01abbcedf7fcba1
Farah065 <farahahmad065@gmail.com>
2024-01-30 23:50:49 +0200

    created contact page layout, and changed title and favicon

--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -3,13 +3,30 @@ import Avatar from './Images/Avatar.jpg';
 function Contact() {
     return (
         <div id='contact'>
-            <div className='avatar-border'>
-                <img src={Avatar} className='avatar'/>
+            <div className='contact-text'>
+                <div>
+                    <h1>Get </h1>
+                    <h2>in</h2>
+                </div>
+                <div>
+                    <h1 className='push-right'>Touch</h1>
+                </div>
             </div>
-            <div className='intro-text'>
-                <h2>Hey! I'm</h2>
-                <h1>Contact</h1>
+            <div className='form-outline'>
+                <div className='form-container'>
+                    <form>
+                        <label>Name</label>
+                        <input placeholder='Type here...' required size={50} />
+                        <label>Email</label>
+                        <input placeholder='Type here...' type='email' required />
+                        <label>Type of Enquiry</label>
+                        <input />
+                        <label>Message</label>
+                        <input placeholder='Type here...' required />
+                    </form>
+                </div>
             </div>
+            <p className='copyright'>Farah • © 2024</p>
         </div>
     );
 }
